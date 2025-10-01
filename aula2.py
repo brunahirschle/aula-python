f06c065e10f7295a815e36450702b6ecdc24df9e
bruna <brunaahhirschle@gmail.com>
2025-10-01 10:20:50 -0300

    condicionais

---
+++ aula2.py
@@ -0,0 +1,8 @@
+numero = int(input('Insira um numero: '))
+
+if numero > 0:
+    print(f'o numero {numero} é positivo')
+elif numero < 0:
+    print(f'o numero {numero} é negativo')
+else:
+    print(f'o numero {numero} é zero')
