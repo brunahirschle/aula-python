96a2c9a5cbac73f72b6ec9faf6d1be6ece6edb88
bruna <brunaahhirschle@gmail.com>
2025-10-28 16:42:30 -0300

    dicionarios e sets

---
+++ aula2.2.py
@@ -0,0 +1,11 @@
+contato = {
+    'nome': '',
+    'telefone': 0,
+    'email': ''
+}
+
+contato['nome'] = input('Digite o nome: ')
+contato['telefone'] = input('Digite o telefone: ')
+contato['email'] = input('Digite o email: ')
+
+print(contato)
