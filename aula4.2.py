1873fb464ad10a892b8d75ffcbd106204442fe0e
bruna <brunaahhirschle@gmail.com>
2025-11-12 21:14:40 -0300

    funcoes I

---
+++ aula4.2.py
@@ -0,0 +1,8 @@
+def media(num1, num2, num3):
+    return (num1 + num2 + num3)/3
+
+numero1 = float(input('Digite o primeiro numero: '))
+numero2 = float(input('Digite o segundo numero: '))
+numero3 = float(input('Digite o terceiro numero: '))
+Media = media(numero1, numero2, numero3)
+print(Media)
